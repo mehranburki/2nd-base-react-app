d87de1ba7c08e6db657026b39f6e7716bb47c861
Mehran Khan <mehranburki@gmail.com>
2020-12-02 11:34:55 +0500

    checking deploy.yaml

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>mehran khan<App /></div>
   </React.StrictMode>,
   document.getElementById('root')
 );
